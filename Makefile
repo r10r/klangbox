32792516ce1d8dccbd4e56723bb9a258ed7b70e9
Ruben Jenster <rjenster@gmail.com>
2014-06-19 08:47:22 +0200

    Implemented hello world module.

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 #LDFLAGS := -lpulse
-# Build is for user account 
-CFLAGS := -I/usr/local/src/pulseaudio/src
+# Build is for user account
+CFLAGS := -Wall -Werror -I/usr/local/src/pulseaudio/src
 
-default: module-hello-world.so
+default: module-hello-world
 
-module-hello-world.so:
-	gcc $(CFLAGS) -g -shared -o module-hello-world.so module-hello-world.c
+module-hello-world: module-hello-world.c
+	gcc $(CFLAGS) -g -shared -o $@.so $?
+
+pulsedevicelist: pulsedevicelist.c
+	gcc $(CFLAGS) -o $@ $? -lpulse
+
+install: module-hello-world
+	sudo cp module-hello-world.so /usr/lib/pulse-5.0/modules
--- module-hello-world.c
+++ module-hello-world.c
@@ -1,15 +1,24 @@
-#include "../config.h"
+#include "../config.h"	/* generated from configure script */
 #include "pulsecore/module.h"
 #include "pulsecore/log.h"
 #include "pulsecore/modargs.h"
+#include "pulsecore/core.h"
+#include "pulsecore/core-util.h"
 
-static const char** g_argument_keys = {"foo", "bar"};
+static const char* const g_argument_keys[] = {"foo", "bar", NULL};
 
 #define INIT_SUCCESS 0
 #define INIT_FAIL -1
 
-int pa__init(pa_module* m)
+PA_MODULE_AUTHOR("Ruben Jenster");
+PA_MODULE_DESCRIPTION("Simple hello world module");
+PA_MODULE_LOAD_ONCE(1);
+PA_MODULE_VERSION("0.0.1");
+
+int
+pa__init(pa_module* m)
 {
+	// TODO store parsed arguments in userdata ?
 	pa_modargs* modargs = pa_modargs_new(m->argument, g_argument_keys);
 	if (!modargs)
 	{
@@ -17,6 +26,21 @@ int pa__init(pa_module* m)
 		return INIT_FAIL;
 	}
 
+	// to check whether pulse is running in system mode
+	if (pa_in_system_mode())
+	{
+		pa_log_notice("PA really is indeed in system mode");
+	}
+
+	// entry point to the core API is m->core
+//	m->core;
+
 	pa_log_notice("Initialized module %s : arguments[%s]", m->name, m->argument);
   return INIT_SUCCESS;
+}
+
+void
+pa__done(pa_module* m)
+{
+	pa_log_notice("Destroyed module %s", m->name);
 }
